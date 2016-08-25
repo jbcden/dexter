364a6b4188bf56d13081d03fb46e1ebcc22a067e
Juan Soto <juan@juansoto.me>
2016-08-25 11:00:55 -0400

    1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dexter.Mixfile do
   def project do
     [
       app: :dexter,
-     version: "0.0.0",
+     version: "1.0.0",
      elixir: "~> 1.3",
      description: description,
      package: package,
